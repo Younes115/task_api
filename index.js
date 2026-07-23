2b7193354ce303e346c0b8722f1ecdc391a8259e
Younes Aiman <y01094779205@gmail.com>
2026-07-23 22:44:07 +0300

    Stage 4: full CRUD

--- index.js
+++ index.js
@@ -46,6 +46,32 @@ app.post("/tasks", (req, res) => {
     res.status(201).json(task);
 });
 
+
+app.put("/tasks/:id", (req, res) => {
+    const taskId = parseInt(req.params.id);
+    const task = tasks.find(t => t.id === taskId);
+    if (!task) {
+        return res.status(404).json({ error: "Task not found" });
+    }
+    const { title, done } = req.body;
+    if (!title || typeof title !== 'string'|| title.trim() === '') {
+        return res.status(400).json({ error: "Title is required and cannot be empty" });
+    }
+
+    if (title) task.title = title;
+    if (done !== undefined) task.done = done;
+    res.json(task);
+});
+
+app.delete('/tasks/:id', (req, res) => {
+    const taskId = parseInt(req.params.id);
+    const taskIndex = tasks.findIndex(t => t.id === taskId);
+    if (taskIndex === -1) {
+        return res.status(404).json({ error: "Task not found" });
+    }
+    tasks.splice(taskIndex, 1);
+    res.json({ message: "Task deleted successfully" });
+});
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
