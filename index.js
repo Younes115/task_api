f551a1bcb91afc76a6cf13144d3ec76a24da37c0
Younes Aiman <y01094779205@gmail.com>
2026-08-01 19:54:39 +0300

    "Stage 3: update and delete with SQL"

--- index.js
+++ index.js
@@ -94,29 +94,65 @@ app.post("/tasks", (req, res) => {
 
 
 app.put("/tasks/:id", (req, res) => {
-    const taskId = parseInt(req.params.id);
-    const task = tasks.find(t => t.id === taskId);
-    if (!task) {
-        return res.status(404).json({ error: "Task not found" });
-    }
+   const taskId = parseInt(req.params.id);
     const { title, done } = req.body;
-    if (!title || typeof title !== 'string'|| title.trim() === '') {
-        return res.status(400).json({ error: "Title is required and cannot be empty" });
+
+    if (title === undefined && done === undefined) {
+        return res.status(400).json({ "error": "Empty or invalid body" });
     }
 
-    if (title) task.title = title;
-    if (done !== undefined) task.done = done;
-    res.json(task);
+    const getStmt = db.prepare('SELECT * FROM tasks WHERE id = ?');
+    const task = getStmt.get(taskId);
+
+ 
+    if (!task) {
+        return res.status(404).json({ "error": `Task ${taskId} not found` });
+    }
+
+    
+    let newTitle = task.title;
+    let newDone = task.done;
+
+    if (title !== undefined) {
+        if (typeof title !== 'string' || title.trim() === '') {
+            return res.status(400).json({ "error": "Title cannot be empty" });
+        }
+        newTitle = title.trim();
+    }
+
+    if (done !== undefined) {
+        newDone = done ? 1 : 0; 
+    }
+
+   
+    const updateStmt = db.prepare('UPDATE tasks SET title = ?, done = ? WHERE id = ?');
+    updateStmt.run(newTitle, newDone, taskId);
+
+    
+    res.json({
+        id: taskId,
+        title: newTitle,
+        done: newDone === 1
+    });
 });
 
 app.delete('/tasks/:id', (req, res) => {
-    const taskId = parseInt(req.params.id);
-    const taskIndex = tasks.findIndex(t => t.id === taskId);
-    if (taskIndex === -1) {
-        return res.status(404).json({ error: "Task not found" });
+   const taskId = parseInt(req.params.id);
+
+    const getStmt = db.prepare('SELECT * FROM tasks WHERE id = ?');
+    const task = getStmt.get(taskId);
+
+   
+    if (!task) {
+        return res.status(404).json({ "error": `Task ${taskId} not found` });
     }
-    tasks.splice(taskIndex, 1);
-    res.json({ message: "Task deleted successfully" });
+
+  
+    const deleteStmt = db.prepare('DELETE FROM tasks WHERE id = ?');
+    deleteStmt.run(taskId);
+
+   
+    res.status(204).send();
 });
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
